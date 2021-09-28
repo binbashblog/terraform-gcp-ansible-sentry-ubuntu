6de47c030615a077f2bf0c6af78f1d673cf7605e
binbashblog <harpinder@sanghera.me.uk>
2021-09-28 02:25:01 +0000

    fixed issue with ip not outputting

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
   output "ip" {
-    value = "self.network_interface.0.access_config.0.nat_ip"
+    value = "${google_compute_instance.terraform-gcp-sentry-ubuntu.network_interface.0.access_config.0.nat_ip}"
   }
